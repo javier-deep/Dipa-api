2275bd2ad26a72d8e865911a06260f4da3f97dc1
javier-deep <moralesriosgerardojaviermorale@gmail.com>
2025-07-25 11:41:41 -0600

    Agregado jsonwebtoken

--- db/connection.js
+++ db/connection.js
@@ -6,13 +6,14 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306  // <-- Agrega esta línea
 });
 
 db.connect((err) => {
   if (err) {
     console.error('❌ Error al conectar a la base de datos:', err);
-    process.exit(1); // termina la app si falla la conexión
+    process.exit(1);
   }
   console.log('✅ Conexión a la base de datos MySQL exitosa');
 });
